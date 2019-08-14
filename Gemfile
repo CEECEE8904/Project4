eac261a8bd5999b1d86df4667e6fd73463f9ada0
CeeCee <ceecee@CeeCees-MacBook-Air.local>
2019-08-13 20:40:22 -0400

    controllers added

--- Gemfile
+++ Gemfile
@@ -43,3 +43,7 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+
+# Use Json Web Token (JWT) for token based authentication
+gem 'jwt'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_12_210614) do
+ActiveRecord::Schema.define(version: 2019_08_14_003218) do
 
   create_table "collects", force: :cascade do |t|
     t.string "name"
@@ -35,6 +35,14 @@ ActiveRecord::Schema.define(version: 2019_08_12_210614) do
     t.string "review"
     t.string "imgurl"
     t.integer "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
