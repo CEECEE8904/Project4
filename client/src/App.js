626c0bc0bca63d361d9ebeeed8808dd53d81b994
CeeCee <ceecee@CeeCees-MacBook-Air.local>
2019-08-13 21:04:14 -0400

    full auth working

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,120 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route } from 'react-router-dom';
+import { withRouter } from 'react-router';
+import decode from 'jwt-decode';
+import Login from './components/Login'
+import Register from './components/Register'
+
+import {
+  loginUser,
+  registerUser,
+  // readAllUsers,
+  // verifyUser,
+
+} from './services/api-helper'
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentUser: null,
+      authFormData: {
+        username: "",
+        email: "",
+        password: ""
+      }
+    };
+  }
 
-export default App;
+  // async componentDidMount() {
+  //   this.getUsers();
+  //   const user = await verifyUser();
+  //   if (user) {
+  //     this.setState({
+  //       currentUser: user
+  //     })
+  //   }
+  // }
+
+  // getUsers = async () => {
+  //   const users = await readAllUsers();
+  //   this.setState({
+  //     users
+  //   })
+  // }
+
+
+
+
+
+  // -------------- AUTH ------------------
+
+  handleLoginButton = () => {
+    this.props.history.push("/login")
+  }
+
+  handleLogin = async () => {
+    const userData = await loginUser(this.state.authFormData);
+    this.setState({
+      currentUser: userData
+    })
+  }
+
+  handleRegister = async (e) => {
+    e.preventDefault();
+    await registerUser(this.state.authFormData);
+    this.handleLogin();
+  }
+
+  handleLogout = () => {
+    localStorage.removeItem("authToken");
+    this.setState({
+      currentUser: null
+    })
+  }
+
+  authHandleChange = (e) => {
+    const { name, value } = e.target;
+    this.setState(prevState => ({
+      authFormData: {
+        ...prevState.authFormData,
+        [name]: value
+      }
+    }));
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <h1>AUTH TESTING</h1>
+          <div>
+            {this.state.currentUser
+              ?
+              <>
+                <p>{this.state.currentUser.username}</p>
+                <button onClick={this.handleLogout}>Logout</button>
+              </>
+              :
+              <button onClick={this.handleLoginButton}>Login/Register</button>}
+
+          </div>
+        </header>
+        <Route exact path="/login" render={() => (
+          <Login
+            handleLogin={this.handleLogin}
+            handleChange={this.authHandleChange}
+            formData={this.state.authFormData} />)} />
+        <Route exact path="/register" render={() => (
+          <Register
+            handleRegister={this.handleRegister}
+            handleChange={this.authHandleChange}
+            formData={this.state.authFormData} />)} />
+      </div>
+    );
+  }
+}
+export default withRouter(App);
