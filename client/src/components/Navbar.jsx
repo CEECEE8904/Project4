ccc3cc64a5a9527626d28a21173d92b380d341bd
CeeCee <ceecee@CeeCees-MacBook-Air.local>
2019-08-20 19:30:32 -0400

    stylings

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -6,6 +6,9 @@ function Navbar(props) {
     <>
       <nav>
         <div className="navbar">
+          <div className="logo_nav">
+            {/* <a href="/home"><img src={require('./images/logo_final.png')} style={( width:145, marginTop: -7)} /></a> */}
+          </div>
           <a href="/home" className="logo">SNKR HEDZ</a>
           <ul className="nav">
             <li><Link to="/home">Home</Link></li>
